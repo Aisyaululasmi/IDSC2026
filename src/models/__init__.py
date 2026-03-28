0f484adf12b207e4ee898b66360fa45383fad4ab
FIQKI HAIDAR AMRULLOH <89396542+FiqkiH@users.noreply.github.com>
2026-03-28 20:55:53 +0700

    add cnn1d, and recurrent-based model to init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,15 @@
 from .resnet1d import ResNet1D, ResidualBlock1D
+from .cnn1d import CNN1D, ConvBlock1D
+from .rnn1d import RNN1D
+from .bilstm1d import BiLSTM1D
+from .lstm1d import LSTM1D
 
-__all__ = ["ResNet1D", "ResidualBlock1D"]
+__all__ = [
+    "ResNet1D",
+    "ResidualBlock1D",
+    "CNN1D",
+    "ConvBlock1D",
+    "RNN1D",
+    "BiLSTM1D",
+    "LSTM1D",
+]
